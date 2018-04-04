005073901861bb422fb308d8ac28836a2945e6dc
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-04-04 09:58:18 -0500

    be less clever

--- SignInHttpTrigger/index.js
+++ SignInHttpTrigger/index.js
@@ -2,17 +2,21 @@ exports.signInProcess = (context, req) => {
 
   resposne = "!!!!!!!!"
   
-  req ?
+  if (req) {
+    context.log(req.body);
+
     context.res = {
       "body": {
         "speech": response, 
         "displayText": response 
       }
-    } :
+    } 
+  } else {
     context.log.error('Fuck!')
+  } 
 
-  context.log(process.version);
-  context.log(context.res.body);
+  // context.log(process.version);
+  // context.log(context.res.body);
   context.done();
 
 };
