eff4c647b7aa3a58525c7e91511ae16dc4c6c759
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-04-04 13:56:46 -0500

    add json parse

--- SignInHttpTrigger/index.js
+++ SignInHttpTrigger/index.js
@@ -3,19 +3,19 @@ exports.signInProcess = (context, req) => {
   response = "!!!!!!!!"
 
   if (req) {
-    const intent = req.body.result.metadata['intentName']
-    context.log(intent)
+    const data = JSON.parse(req.body); //.result.metadata['intentName']
+    context.log(intent);
     context.res
       .status(200)
       .json({
         "speech": response,
         "displayText": response
-      })
+      });
   } else {
     context.res
       .status(400)
-      .send("You suck, this is an error haha")
-    context.log.error('Fuck!')
+      .send("You suck, this is an error haha");
+    context.log.error('Fuck!');
   }
 
   // context.log(process.version);
