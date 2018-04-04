624be62e8570e5a4a4c75bd578befef1612654ea
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-04-04 10:03:00 -0500

    fix response variable (was undefined)

--- SignInHttpTrigger/index.js
+++ SignInHttpTrigger/index.js
@@ -1,22 +1,25 @@
 exports.signInProcess = (context, req) => {
 
-  resposne = "!!!!!!!!"
-  
-  if (req) {
-    context.log(req.body);
+  response = "!!!!!!!!"
 
-    context.res = {
-      "body": {
-        "speech": response, 
-        "displayText": response 
-      }
-    } 
+  if (req) {
+    // data = JSON.parse(req.body)
+    context.log(req.body);
+    context.res
+      .status(200)
+      .json({
+        "speech": response,
+        "displayText": response
+      })
   } else {
+    context.res
+      .status(400)
+      .send("You suck, this is an error haha")
     context.log.error('Fuck!')
-  } 
+  }
 
   // context.log(process.version);
   // context.log(context.res.body);
-  context.done();
+  // context.done();
 
 };
