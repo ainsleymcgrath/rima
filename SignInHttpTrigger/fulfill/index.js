ce4d7b23f15f05378f34af21f4dd9f74dddedb68
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-04-05 11:45:48 -0500

    add SignIn hotword to intent structure

--- SignInHttpTrigger/fulfill/index.js
+++ SignInHttpTrigger/fulfill/index.js
@@ -3,6 +3,9 @@ const fulfill = (intent) => {
     case 'TestIntent':
       return `(With up so floating many bells down)`;
       break;
+    case 'SignIn':
+      return `Awesome. You sign in.`;
+      break;
     default:
       return `Something seems to have gone wrong... Please, try again.`;
       break;
