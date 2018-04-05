0ad07984d534e805c7936ccaea86e30632c6b8fc
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-04-05 12:41:35 -0500

    add todos for checking valid names

--- SignInHttpTrigger/fulfill/index.js
+++ SignInHttpTrigger/fulfill/index.js
@@ -12,6 +12,9 @@ const fulfill = (intent, result) => {
       return `(With up so floating many bells down)`;
       break;
     case 'SignIn':
+      //TODO: create a check for valid name, email
+      //TODO: if `original` params are present in context, 
+      //      prefer those in cases of invalid updates
       return `Awesome. You're signed in as ${parameters['given-name']} with email address ${parameters['email']}.`;
       break;
     default:
