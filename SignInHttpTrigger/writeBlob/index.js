3477235703941bc5e3fb4641a6098ce3cf3427ba
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-04-06 16:24:30 -0500

    comment out changes for barbaric rollback

---
+++ SignInHttpTrigger/writeBlob/index.js
@@ -0,0 +1,17 @@
+// TODO: make this work, you neanderthal
+// require('dotenv').config();
+
+// const writeBlob = (container, obj) => {
+//   const azure = require('azure-storage');
+//   const blobService = azure.createBlobService(
+//     process.env.AZURE_STORAGE_ACCOUNT_NAME,
+//     process.env.AZURE_ACCOUNT_KEY
+//   );
+
+//   blobService.createBlockBlobFromText(
+//     container,
+
+//   )
+// }
+
+// module.exports = writeBlob;
--- SignInHttpTrigger/writeBlobs/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const azure = require('azure-storage');
-const blobService = azure.createBlobService(
-    process.env.AZURE_STORAGE_ACCOUNT_NAME,
-    process.env.AZURE_ACCOUNT_KEY
-);
