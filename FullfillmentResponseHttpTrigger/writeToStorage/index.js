e656cd30b2a93552b5d3a05723cc0bf71a05446e
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-04-21 13:49:38 -0500

    add timer for table service

--- FullfillmentResponseHttpTrigger/writeToStorage/index.js
+++ FullfillmentResponseHttpTrigger/writeToStorage/index.js
@@ -8,22 +8,26 @@ const writeToStorage = (obj) => {
     contexts,
     metadata
   } = obj; // TODO: refactor obj as raw req rather than stupid results obj
+
+  console.time();
   const tableService = azure.createTableService(
     process.env['AZURE_STORAGE_ACCOUNT_NAME'],
     process.env['AZURE_ACCOUNT_KEY']);
+  console.timeEnd();
+    
   const entGen = azure.TableUtilities.entityGenerator;
 
-  const entity = {
-    PartitionKey: entGen.String('Test'),
-    RowKey: entGen.Double('666'),
-    Data: entGen.String(JSON.stringify([parameters, contexts, metadata]))
-  };
+  // const entity = {
+  //   PartitionKey: entGen.String('Test'),
+  //   RowKey: entGen.Double('666'),
+  //   Data: entGen.String(JSON.stringify([parameters, contexts, metadata]))
+  // };
 
-  tableService.insertEntity('AdsfBotTableStorage', entity, (error, result, response) => {
-    if (err) {
-      // bleh
-    }
-  });
+  // tableService.insertEntity('AdsfBotTableStorage', entity, (error, result, response) => {
+  //   if (err) {
+  //     // bleh
+  //   }
+  // });
   
 }
 
