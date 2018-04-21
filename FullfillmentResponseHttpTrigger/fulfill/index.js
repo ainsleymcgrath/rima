062b4a3f34a415bbdd810f099fcd1ae8d4014ca2
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-04-21 13:56:48 -0500

    checking context object keys

--- FullfillmentResponseHttpTrigger/fulfill/index.js
+++ FullfillmentResponseHttpTrigger/fulfill/index.js
@@ -20,7 +20,7 @@ const fulfill = (intent, obj) => {
       //      prefer those in cases of invalid updates
     
       // context.log(`${blobService}, ${typeof blobService}`)
-      writeToStorage(obj);
+      // writeToStorage(obj);
       
       return `Awesome. You're signed in as ${parameters['given-name']} with email address ${parameters['email']}.
               Would you be okay with answering a few more questions?`;
--- FullfillmentResponseHttpTrigger/index.js
+++ FullfillmentResponseHttpTrigger/index.js
@@ -1,36 +1,38 @@
-const fulfill = require('./fulfill')
+// const fulfill = require('./fulfill')
 
 exports.signInProcess = (context, req) => {
-  if (req) {
-    const intent = req.result.metadata.intentName
-    const {
-      resolvedQuery,
-      action,
-      parameters,
-      contexts
-    } = req.result
+  // if (req) {
+  //   const intent = req.result.metadata.intentName
+  //   const {
+  //     resolvedQuery,
+  //     action,
+  //     parameters,
+  //     contexts
+  //   } = req.result
 
-    context.log(`
-      Query: ${resolvedQuery}
-      Action: ${action}
-      Params: ${JSON.stringify(parameters)}
-      Contexts: ${JSON.stringify(contexts)}
-    `);
+  //   context.log(Object.keys(context))
 
-    context.res
-      .status(200)
-      .json({
-        "speech": fulfill(intent, req.result), // TODO: use raw req
-        "displayText": fulfill(intent, req.result) // TODO: use raw req
-      });
-  } else {
-    context.res
-      .status(400)
-      .json({
-        "speech": `Some kind of error occured. Server stuff. Let's give this another shot?`,
-        "displayText": `Some kind of error occured. Server stuff. Let's give this another shot?`
-      });
-      
-    context.log.error(`Error occurred for fulfillment of intent ${intent}`); 
-  }
+  //   // context.log(`
+  //   //   Query: ${resolvedQuery}
+  //   //   Action: ${action}
+  //   //   Params: ${JSON.stringify(parameters)}
+  //   //   Contexts: ${JSON.stringify(contexts)}
+  //   // `);
+
+  //   context.res
+  //     .status(200)
+  //     .json({
+  //       "speech": fulfill(intent, req.result), // TODO: use raw req
+  //       "displayText": fulfill(intent, req.result) // TODO: use raw req
+  //     });
+  // } else {
+  //   context.res
+  //     .status(400)
+  //     .json({
+  //       "speech": `Some kind of error occured. Server stuff. Let's give this another shot?`,
+  //       "displayText": `Some kind of error occured. Server stuff. Let's give this another shot?`
+  //     });
+
+  //   context.log.error(`Error occurred for fulfillment of intent ${intent}`); 
+  // }
 };
