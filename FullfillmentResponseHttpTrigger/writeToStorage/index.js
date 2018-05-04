ce9410629bf81afa998966fe3b6db8a1037c0eeb
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-05-04 10:38:45 -0500

    light refactor of writeToStorage()

--- FullfillmentResponseHttpTrigger/writeToStorage/index.js
+++ FullfillmentResponseHttpTrigger/writeToStorage/index.js
@@ -1,13 +1,13 @@
 const writeToStorage = (obj, context) => {
-  const { contexts, metadata } = obj.result; 
+  const { contexts,  metadata } = obj.result;
   context.bindings.tableBinding = [];
 
   context.bindings.tableBinding.push({
-    PartitionKey: `${metadata.intentName}-${contexts}`,
+    PartitionKey: `${metadata.intentName}`,
     RowKey: obj.id,
     Data: JSON.stringify(obj.result)
   });
-  
+
 }
 
 module.exports = writeToStorage;
