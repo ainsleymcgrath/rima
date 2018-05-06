2974c5aba8a5858a460dde24bbc2db9afcef38e1
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-05-06 18:28:56 -0500

    Add fulfillment for VisitRobotRace intent

--- FullfillmentResponseHttpTrigger/fulfill/index.js
+++ FullfillmentResponseHttpTrigger/fulfill/index.js
@@ -7,25 +7,81 @@ const fulfill = (intent, obj, context) => {
     contexts
   } = obj.result;
 
+  // Used as a lookup to get the correct text response for 
+  // each intent. Handles the game and signin. Another module
+  // handles intents that have non-plaintext payloads.
   switch (intent) {
     case 'TestIntent':
       return `(With up so floating many bells down)`;
       break;
 
-    case 'SignIn':
-      writeToStorage(obj, context);
-      return `Awesome. You're signed in as ${parameters['name']} with email address ${parameters['email']}.
-              Would you be okay with answering a few more questions?`;
-      break;
-
-    case 'SignIn - UserSaysYesToExtraQuestions':
-    case 'AnswerExtraQuestionsOutOfContext':
-      writeToStorage(obj, context);
-      return `Thanks for that. You're helping make the (data) Science Fair even cooler!`;
-      break;
-
     case 'VisitIoTea':
       return `Congratulations, ${parameters['CodeName']}! You just earned 100 points.`
+
+    case 'VisitRobotRace':
+      return `Well played, human. 100 points for ${parameters['CodeName']}!`;
+      break;
+
+    case 'VisitRima':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitSnakes':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitPlumbing':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitScoreboard':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitPinnacle':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitBigSquid':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitAttunity':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitLooker':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitMicrosoft':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitFivetran':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'VisitSnowflake':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'AnswerFavoriteColorQuestion':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'AnswerLeastFavoriteColorQuestion':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'AnswerGender':
+      return `${parameters['CodeName']}`;
+      break;
+
+    case 'AnswerAgeRange':
+      return `${parameters['CodeName']}`;
+      break;
+
   }
 
 };
