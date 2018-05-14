13855546a3ed2384b33c9115a95590bc40363523
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-05-14 17:58:41 -0500

    add logging

--- FullfillmentResponseHttpTrigger/fulfill/index.js
+++ FullfillmentResponseHttpTrigger/fulfill/index.js
@@ -13,12 +13,14 @@ const fulfill = (intent, obj, context) => {
   // call a function to generate the response corresponding to the current intent.
   // decide whether or not that intent needs to be written to storage
   if (intentLookup[intent] == 'TestIntent' || `${intentLookup[intent]}`.indexOf('TellAbout')) {
-    intentLookup[intent].response();
+    context.log(`Rima says: ${intentLookup[intent].response()}`);
+    return intentLookup[intent].response();
   } else if (intentLookup[intent]) {
     writeToStorage(obj, context);
+    context.log(`Rima says: ${intentLookup[intent].response(`${parameters['CodeName']}`)}`);
     return intentLookup[intent].response(`${parameters['CodeName']}`);
   } else {
-    return 'Hmm? Not sure I caught that. Can you try again?'
+    return 'Hmm? Not sure I caught that. Can you try again?';
   }
 };
 
