d3eaa256fdfc5754b1db93476ee609c84ab61f94
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-05-16 21:16:44 -0500

    introduce checks for valid codenames

--- FullfillmentResponseHttpTrigger/fulfill/index.js
+++ FullfillmentResponseHttpTrigger/fulfill/index.js
@@ -1,5 +1,6 @@
 const writeToStorage = require('../writeToStorage');
 const intentLookup = require('../intentLookup');
+const codeNames = require('./codeNames')
 
 const fulfill = (intent, obj, context) => {
   const parameters = obj.result.parameters;
@@ -10,7 +11,10 @@ const fulfill = (intent, obj, context) => {
   // decide whether or not that intent needs to be written to storage
   if (intentLookup[intent]) {
     writeToStorage(obj, context);
-    return intentLookup[intent].response(parameters['CodeName'], parameters['Answer']);
+    // check valid codename before responding
+    return codeNames.includes(parameters['CodeName']) ?
+      intentLookup[intent].response(parameters['CodeName'], parameters['Answer']) 
+      : `That's not a real codename. I don't think I'll trust you until you try again. No funny business this time.`
   } else {
     return 'Hmm? Not sure I caught that. Can you try again?';
   }
